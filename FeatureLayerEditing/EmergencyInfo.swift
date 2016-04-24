f4d26a6dc45754880a802f689548c3a64c5bae28
Matthew Mocniak <mmocniak@macbook-pro-3.local>
2016-04-24 06:47:35 -0400

    Updated with preparation for Snake Information and Emergency Information pages.

---
+++ FeatureLayerEditing/EmergencyInfo.swift
@@ -0,0 +1,37 @@
+//
+//  EmergencyInfo.swift
+//  SnakeEyes
+//
+//  Created by Matthew Mocniak on 4/24/16.
+//  Copyright © 2016 Esri. All rights reserved.
+//
+
+import UIKit
+
+
+func drawImagesAndText() {
+    // 1
+    UIGraphicsBeginImageContextWithOptions(CGSize(width: 512, height: 512), false, 0)
+    let context = UIGraphicsGetCurrentContext()
+    
+    // 2
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.alignment = .Center
+    
+    // 3
+    let attrs = [NSFontAttributeName: UIFont(name: "HelveticaNeue-Thin", size: 36)!, NSParagraphStyleAttributeName: paragraphStyle]
+    
+    // 4
+    let string = "The best-laid schemes o'\nmice an' men gang aft agley"
+    string.drawWithRect(CGRect(x: 32, y: 32, width: 448, height: 448), options: .UsesLineFragmentOrigin, attributes: attrs, context: nil)
+    
+    // 5
+    let mouse = UIImage(named: "mouse")
+    mouse?.drawAtPoint(CGPoint(x: 300, y: 150))
+    
+    // 6
+    let img = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    // 7
+}
